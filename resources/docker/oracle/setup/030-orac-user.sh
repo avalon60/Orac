b7abce83bf67bb607c7dfb51d377c885bbafee6d
avalon60 <cbostock20@gmail.com>
2026-04-25 10:20:15 +0100

    Create Oracle users from setup file

--- resources/docker/oracle/setup/030-orac-user.sh
+++ resources/docker/oracle/setup/030-orac-user.sh
@@ -1,9 +1,10 @@
 # Author: Clive Bostock
-#   Date: 22 Aug 2021
+#   Date: 25 Apr 2026
 #
-# DAPEX script to install APEX on container setup.
+# Orac script to create application schema users during container setup.
 #
-E="-e"
+set -Eeuo pipefail
+
 PROG="Orac: 030-orac-user.sh"
 timestamp() { date +"%Y-%m-%d %H:%M:%S"; }
 echo "[$(timestamp)] ${PROG} Started"
@@ -12,25 +13,58 @@ export ORACLE_SID=${ORACLE_SID:-FREE}
 export ORACLE_PDB=${ORACLE_PDB:-FREEPDB1}
 export ORAC_HOME=${ORAC_HOME:-/home/oracle/orac}
 export ORACLE_BASE=${ORACLE_BASE:-/opt/oracle}
+USER_LIST_FILE="${ORACLE_BASE}/scripts/setup/orac_users.txt"
 
-# Derive the CDN for this release
-CDN=" https://static.oracle.com/cdn/apex/${APEX_VERSION}.0/"
+if [[ ! -f "${USER_LIST_FILE}" ]]; then
+  echo "[$(timestamp)] ${PROG} Missing user list file: ${USER_LIST_FILE}"
+  exit 1
+fi
 
-pushd ${APEX_HOME}
+mapfile -t ORAC_USERS < <(grep -v '^[[:space:]]*$' "${USER_LIST_FILE}")
+
+if [[ ${#ORAC_USERS[@]} -eq 0 ]]; then
+  echo "[$(timestamp)] ${PROG} No users defined in ${USER_LIST_FILE}"
+  exit 1
+fi
+
+pushd "${APEX_HOME}" >/dev/null
 
 echo "${PROG} Started"
-echo "${PROG} Launching sqlplus; installing APEX..."
-sqlplus / as sysdba <<EOF 
+echo "${PROG} Launching sqlplus; creating application users..."
+sqlplus / as sysdba <<EOF
 alter session set container=${ORACLE_PDB};
 
-create user ORAC identified by ${ORACLE_PWD}
-  default tablespace users
-  temporary tablespace temp
-  quota unlimited on users;
+EOF
+
+for user_name in "${ORAC_USERS[@]}"; do
+  echo "[$(timestamp)] ${PROG} Ensuring user ${user_name} exists"
+  sqlplus / as sysdba <<EOF
+alter session set container=${ORACLE_PDB};
+
+declare
+  l_count number;
+begin
+  select count(*)
+    into l_count
+    from dba_users
+   where username = upper('${user_name}');
+
+  if l_count = 0 then
+    execute immediate q'[
+      create user ${user_name} identified by ${ORACLE_PWD}
+        default tablespace users
+        temporary tablespace temp
+        quota unlimited on users
+    ]';
+  end if;
+end;
+/
 
 grant create session, create table, create view, create sequence,
       create procedure, create trigger, create type, create synonym
-to ORAC;
-
+to ${user_name};
 EOF
-echo "[$(timestamp)] ${PROG}: Done." 
+done
+
+popd >/dev/null
+echo "[$(timestamp)] ${PROG}: Done."
